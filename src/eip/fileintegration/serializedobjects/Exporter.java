2c4fa2bd83b1acb14792c832a763273d92a7e8ef
Ernesto <ernesto.exposito@gmail.com>
2020-04-03 14:25:04 +0200

    EIP file integration design pattern based on serialized objects

---
+++ src/eip/fileintegration/serializedobjects/Exporter.java
@@ -0,0 +1,26 @@
+package eip.fileintegration.serializedobjects;
+
+import java.io.FileNotFoundException;
+
+/*
+/**
+ *
+ * @author ernestoexposito
+ */
+public class Exporter {
+
+    public Exporter(String filename) throws FileNotFoundException {
+        
+    }
+
+    public void exportObject(Object data) {
+        
+    }
+    
+    public void close()
+    {
+        
+    }
+
+}
+
---
+++ src/eip/fileintegration/serializedobjects/Importer.java
@@ -0,0 +1,25 @@
+package eip.fileintegration.serializedobjects;
+
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author ernestoexposito
+ */
+public class Importer {
+
+    public Importer(String filename) throws FileNotFoundException {
+       
+    }
+
+    public Object importObject() {
+        
+        return null;
+    }
+    
+     public void close()
+    {
+        
+        
+    }
+}
